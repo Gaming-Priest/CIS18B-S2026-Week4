b8ccbd1a145412a878e21da3799354259955d39c
GamingPriest <kclang02@gmail.com>
2026-03-20 23:50:29 +0000

    finished mini3 custom exceptions

--- week4-exercises/mini03-custom-exception/src/main/java/edu/norcocollege/cis18b/weekx/mini03/AlertLevel.java
+++ week4-exercises/mini03-custom-exception/src/main/java/edu/norcocollege/cis18b/weekx/mini03/AlertLevel.java
@@ -1,6 +1,5 @@
 package edu.norcocollege.cis18b.weekx.mini03;
 
-/* TODO: Make this an enum */
 public enum AlertLevel 
 {
     CRITICAL,
--- week4-exercises/mini03-custom-exception/src/main/java/edu/norcocollege/cis18b/weekx/mini03/AlertValidatorDemo.java
+++ week4-exercises/mini03-custom-exception/src/main/java/edu/norcocollege/cis18b/weekx/mini03/AlertValidatorDemo.java
@@ -1,19 +1,30 @@
 package edu.norcocollege.cis18b.weekx.mini03;
 
-public class AlertValidatorDemo {
-
-    public static void validateAlert(Alert alert) throws InvalidAlertException {
-        // TODO: Throw InvalidAlertException if the message is null or blank.
-        // TODO: Throw InvalidAlertException if the level is null.
+public class AlertValidatorDemo 
+{
+    public static void validateAlert(Alert alert) throws InvalidAlertException 
+    {
+        if (alert.getMessage() == null || alert.getMessage().trim().isEmpty()) 
+        {
+            throw new InvalidAlertException("Alert message cannot be null or blank");
+        }
+        if (alert.getLevel() == null) 
+        {
+            throw new InvalidAlertException("Alert level cannot be null");
+        }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         Alert alert = new Alert(1, "", null);
 
-        try {
+        try 
+        {
             validateAlert(alert);
             System.out.println("Alert is valid.");
-        } catch (InvalidAlertException ex) {
+        } 
+        catch (InvalidAlertException ex) 
+        {
             System.out.println("Invalid alert: " + ex.getMessage());
         }
     }
